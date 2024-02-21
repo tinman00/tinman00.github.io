7c7c350b0b60d538d746a9e4a6248a7c2bd16a0d
tinman00 <117823440+tinman00@users.noreply.github.com>
2024-02-21 11:02:46 +0800

    不做本地测试导致的

--- 10/namerena/md5.js
+++ 10/namerena/md5.js
@@ -8083,7 +8083,7 @@
       } else {
         P.count = 1
       }
-      if (P.count > 1050) return
+      if (P.count > 1100) return
       if (P.count == 1) {
         setTimeout(H.cU(new P.l9(this, b), 0), 0)
       } else {
